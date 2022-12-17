a177cbef6f210f59d21d561b8ce2176c507145ec
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2022-12-17 04:14:01 -0600

    More tests

---
+++ tests/Unit/WorkflowStubTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\Fixtures\TestActivity;
+use Tests\Fixtures\TestOtherActivity;
+use Tests\Fixtures\TestWorkflow;
+use Tests\TestCase;
+use Workflow\Signal;
+use Workflow\States\WorkflowCompletedStatus;
+use Workflow\WorkflowStub;
+
+final class WorkflowStubTest extends TestCase
+{
+    public function testCompleted(): void
+    {
+        $workflow = WorkflowStub::make(TestWorkflow::class);
+
+        $workflow->start(shouldAssert: false);
+
+        $workflow->cancel();
+
+        while (! $workflow->isCanceled());
+
+        while ($workflow->running());
+
+        $this->assertSame(WorkflowCompletedStatus::class, $workflow->status());
+        $this->assertSame('workflow_activity_other', $workflow->output());
+        $this->assertSame([TestActivity::class, TestOtherActivity::class, Signal::class], $workflow->logs()
+            ->pluck('class')
+            ->sort()
+            ->values()
+            ->toArray());
+    }
+}
