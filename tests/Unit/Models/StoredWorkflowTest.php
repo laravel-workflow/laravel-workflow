b1bd40170422edd72a11558fa1feef9d18c627b3
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2022-12-17 14:51:15 -0600

    More tests

--- tests/Unit/Models/StoredWorkflowTest.php
+++ tests/Unit/Models/StoredWorkflowTest.php
@@ -9,7 +9,7 @@ use Workflow\Models\StoredWorkflow;
 use Workflow\Models\StoredWorkflowException;
 use Workflow\Models\StoredWorkflowLog;
 use Workflow\Models\StoredWorkflowSignal;
-use Workflow\Models\StoredWorkflowTime;
+use Workflow\Models\StoredWorkflowTimer;
 
 final class StoredWorkflowTest extends TestCase
 {
@@ -45,18 +45,30 @@ final class StoredWorkflowTest extends TestCase
 
         $this->assertSame(1, $workflow->exceptions()->count());
         $this->assertSame($workflow->id, $workflow->exceptions()->first()->stored_workflow_id);
-        $this->assertSame($workflow->exceptions()->first()->id, StoredWorkflowException::whereStoredWorkflowId($workflow->id)->first()->id);
+        $this->assertSame(
+            $workflow->exceptions()->first()->id,
+            StoredWorkflowException::whereStoredWorkflowId($workflow->id)->first()->id
+        );
 
         $this->assertSame(1, $workflow->logs()->count());
         $this->assertSame($workflow->id, $workflow->logs()->first()->stored_workflow_id);
-        $this->assertSame($workflow->logs()->first()->id, StoredWorkflowLog::whereStoredWorkflowId($workflow->id)->first()->id);
+        $this->assertSame(
+            $workflow->logs()->first()->id,
+            StoredWorkflowLog::whereStoredWorkflowId($workflow->id)->first()->id
+        );
 
         $this->assertSame(1, $workflow->signals()->count());
         $this->assertSame($workflow->id, $workflow->signals()->first()->stored_workflow_id);
-        $this->assertSame($workflow->signals()->first()->id, StoredWorkflowSignal::whereStoredWorkflowId($workflow->id)->first()->id);
+        $this->assertSame(
+            $workflow->signals()->first()->id,
+            StoredWorkflowSignal::whereStoredWorkflowId($workflow->id)->first()->id
+        );
 
         $this->assertSame(1, $workflow->timers()->count());
         $this->assertSame($workflow->id, $workflow->timers()->first()->stored_workflow_id);
-        $this->assertSame($workflow->timers()->first()->id, StoredWorkflowException::whereStoredWorkflowId($workflow->id)->first()->id);
+        $this->assertSame(
+            $workflow->timers()->first()->id,
+            StoredWorkflowTimer::whereStoredWorkflowId($workflow->id)->first()->id
+        );
     }
 }
