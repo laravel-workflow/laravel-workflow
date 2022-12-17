41648189b435603988890e1c3e6e6847e6e3ed3e
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2022-12-16 21:46:34 -0600

    More tests

---
+++ tests/Unit/Middleware/WorkflowMiddlewareTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Middleware;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Queue;
+use Mockery\MockInterface;
+use Tests\Fixtures\TestActivity;
+use Tests\Fixtures\TestWorkflow;
+use Tests\TestCase;
+use Workflow\Middleware\WorkflowMiddleware;
+use Workflow\Models\StoredWorkflow;
+use Workflow\States\WorkflowCompletedStatus;
+use Workflow\States\WorkflowRunningStatus;
+use Workflow\States\WorkflowWaitingStatus;
+use Workflow\WorkflowStub;
+
+final class WorkflowMiddlewareTest extends TestCase
+{
+    public function testMiddleware(): void
+    {
+        Queue::fake();
+
+        $workflow = WorkflowStub::make(TestWorkflow::class);
+        $workflow->start();
+
+        $storedWorkflow = StoredWorkflow::findOrFail($workflow->id());
+        $storedWorkflow->update(['status' => WorkflowWaitingStatus::class]);
+
+        $activity = $this->mock(TestActivity::class);
+        $activity->index = 0;
+        $activity->now = now()->toDateTimeString();
+        $activity->storedWorkflow = $storedWorkflow;
+
+        $middleware = new WorkflowMiddleware();
+
+        $middleware->handle($activity, function ($job) {
+            return true;
+        });
+
+        Queue::assertPushed(TestWorkflow::class, 2);
+    }
+
+    public function testAlreadyCompleted(): void
+    {
+        Queue::fake();
+
+        $workflow = WorkflowStub::make(TestWorkflow::class);
+        $workflow->start();
+
+        $storedWorkflow = StoredWorkflow::findOrFail($workflow->id());
+        $storedWorkflow->update(['status' => WorkflowCompletedStatus::class]);
+
+        $activity = $this->mock(TestActivity::class);
+        $activity->index = 0;
+        $activity->now = now()->toDateTimeString();
+        $activity->storedWorkflow = $storedWorkflow;
+
+        $middleware = new WorkflowMiddleware();
+
+        $middleware->handle($activity, function ($job) {
+            return true;
+        });
+
+        Queue::assertPushed(TestWorkflow::class, 1);
+    }
+
+    public function testAlreadyRunning(): void
+    {
+        Queue::fake();
+
+        $workflow = WorkflowStub::make(TestWorkflow::class);
+        $workflow->start();
+
+        $storedWorkflow = StoredWorkflow::findOrFail($workflow->id());
+        $storedWorkflow->update(['status' => WorkflowRunningStatus::class]);
+
+        $activity = $this->mock(TestActivity::class, static function (MockInterface $mock) {
+            $mock->shouldReceive('release')
+                ->once();
+        });
+        $activity->index = 0;
+        $activity->now = now()->toDateTimeString();
+        $activity->storedWorkflow = $storedWorkflow;
+
+        $middleware = new WorkflowMiddleware();
+
+        $middleware->handle($activity, function ($job) {
+            return true;
+        });
+
+        Queue::assertPushed(TestWorkflow::class, 1);
+    }
+}
