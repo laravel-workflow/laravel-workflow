e318dfe13fdb358979b712fbd20e104205096dce
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2022-12-17 04:57:09 -0600

    More tests

--- tests/Unit/WorkflowStubTest.php
+++ tests/Unit/WorkflowStubTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use Exception;
+use Illuminate\Support\Carbon;
 use Tests\Fixtures\TestWorkflow;
 use Tests\TestCase;
+use Workflow\States\WorkflowFailedStatus;
 use Workflow\States\WorkflowPendingStatus;
 use Workflow\WorkflowStub;
 
@@ -13,7 +16,9 @@ final class WorkflowStubTest extends TestCase
 {
     public function testMake(): void
     {
-        $workflow = WorkflowStub::make(TestWorkflow::class);
+        Carbon::setTestNow('2022-01-01');
+
+        $workflow = WorkflowStub::load(WorkflowStub::make(TestWorkflow::class)->id());
 
         $workflow->start();
 
@@ -21,6 +26,17 @@ final class WorkflowStubTest extends TestCase
 
         while (! $workflow->isCanceled());
 
+        $this->assertSame('2022-01-01 00:00:00', WorkflowStub::now()->toDateTimeString());
+
+        $workflow = $workflow->fresh();
+
+        $this->assertSame(WorkflowPendingStatus::class, $workflow->status());
+        $this->assertNull($workflow->output());
+
+        $workflow->fail(new Exception('test'));
+        $this->assertSame(WorkflowFailedStatus::class, $workflow->status());
+
+        $workflow->restart();
         $this->assertSame(WorkflowPendingStatus::class, $workflow->status());
     }
 }
