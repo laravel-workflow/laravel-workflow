0e08e8292514b911e7b8edb6279d79e756cdab64
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2022-12-24 18:01:15 -0600

    Add more tests

--- src/Traits/AwaitWithTimeouts.php
+++ src/Traits/AwaitWithTimeouts.php
@@ -41,7 +41,14 @@ trait AwaitWithTimeouts
                             'result' => Y::serialize($result),
                         ]);
                 } catch (QueryException $exception) {
-                    // already logged
+                    $log = self::$context->storedWorkflow->logs()
+                        ->whereIndex(self::$context->index)
+                        ->first();
+
+                    if ($log) {
+                        ++self::$context->index;
+                        return resolve(Y::unserialize($log->result));
+                    }
                 }
             }
             ++self::$context->index;
---
+++ tests/Unit/Traits/AwaitWithTimeoutsTest.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Traits;
+
+use Tests\Fixtures\TestWorkflow;
+use Tests\TestCase;
+use Workflow\Models\StoredWorkflow;
+use Workflow\Serializers\Y;
+use Workflow\Signal;
+use Workflow\States\WorkflowPendingStatus;
+use Workflow\WorkflowStub;
+
+final class AwaitWithTimeoutsTest extends TestCase
+{
+    public function testDefersIfNoResult(): void
+    {
+        $workflow = WorkflowStub::load(WorkflowStub::make(TestWorkflow::class)->id());
+        $storedWorkflow = StoredWorkflow::findOrFail($workflow->id());
+        $storedWorkflow->update([
+            'arguments' => Y::serialize([]),
+            'status' => WorkflowPendingStatus::$name,
+        ]);
+
+        WorkflowStub::awaitWithTimeout(60, static fn () => false)
+            ->then(static function ($value) use (&$result) {
+                $result = $value;
+            });
+
+        $this->assertNull($result);
+        $this->assertSame(0, $workflow->logs()->count());
+
+        WorkflowStub::awaitWithTimeout('1 minute', static fn () => false)
+            ->then(static function ($value) use (&$result) {
+                $result = $value;
+            });
+
+        $this->assertNull($result);
+        $this->assertSame(0, $workflow->logs()->count());
+    }
+
+    public function testStoresResult(): void
+    {
+        $workflow = WorkflowStub::load(WorkflowStub::make(TestWorkflow::class)->id());
+
+        WorkflowStub::awaitWithTimeout('1 minute', static fn () => true)
+            ->then(static function ($value) use (&$result) {
+                $result = $value;
+            });
+
+        $this->assertSame(true, $result);
+        $this->assertSame(1, $workflow->logs()->count());
+        $this->assertDatabaseHas('workflow_logs', [
+            'stored_workflow_id' => $workflow->id(),
+            'index' => 0,
+            'class' => Signal::class,
+            'result' => Y::serialize(true),
+        ]);
+    }
+
+    public function testLoadsStoredResult(): void
+    {
+        $workflow = WorkflowStub::load(WorkflowStub::make(TestWorkflow::class)->id());
+        $storedWorkflow = StoredWorkflow::findOrFail($workflow->id());
+        $storedWorkflow->logs()
+            ->create([
+                'index' => 0,
+                'now' => WorkflowStub::now(),
+                'class' => Signal::class,
+                'result' => Y::serialize(true),
+            ]);
+
+        WorkflowStub::awaitWithTimeout('1 minute', static fn () => true)
+            ->then(static function ($value) use (&$result) {
+                $result = $value;
+            });
+
+        $this->assertSame(true, $result);
+        $this->assertSame(1, $workflow->logs()->count());
+        $this->assertDatabaseHas('workflow_logs', [
+            'stored_workflow_id' => $workflow->id(),
+            'index' => 0,
+            'class' => Signal::class,
+            'result' => Y::serialize(true),
+        ]);
+    }
+
+    public function testResolvesConflictingResult(): void
+    {
+        $workflow = WorkflowStub::load(WorkflowStub::make(TestWorkflow::class)->id());
+
+        WorkflowStub::awaitWithTimeout('1 minute', static function () use ($workflow) {
+            $storedWorkflow = StoredWorkflow::findOrFail($workflow->id());
+            $storedWorkflow->logs()
+                ->create([
+                    'index' => 0,
+                    'now' => WorkflowStub::now(),
+                    'class' => Signal::class,
+                    'result' => Y::serialize(false),
+                ]);
+            return true;
+        })
+            ->then(static function ($value) use (&$result) {
+                $result = $value;
+            });
+
+        $this->assertSame(false, $result);
+        $this->assertSame(1, $workflow->logs()->count());
+        $this->assertDatabaseHas('workflow_logs', [
+            'stored_workflow_id' => $workflow->id(),
+            'index' => 0,
+            'class' => Signal::class,
+            'result' => Y::serialize(false),
+        ]);
+    }
+}
