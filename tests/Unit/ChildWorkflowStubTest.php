974a76560934512771dbf875ff3a33da670fc19d
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-03-23 21:40:26 +0000

    Add test

--- tests/Unit/ChildWorkflowStubTest.php
+++ tests/Unit/ChildWorkflowStubTest.php
@@ -58,6 +58,36 @@ final class ChildWorkflowStubTest extends TestCase
         $this->assertSame('test', $result);
     }
 
+    public function testLoadsChildWorkflow(): void
+    {
+        $workflow = WorkflowStub::load(WorkflowStub::make(TestParentWorkflow::class)->id());
+        $storedWorkflow = StoredWorkflow::findOrFail($workflow->id());
+        $storedWorkflow->update([
+            'arguments' => Y::serialize([]),
+            'status' => WorkflowPendingStatus::$name,
+        ]);
+
+        $childWorkflow = WorkflowStub::load(WorkflowStub::make(TestChildWorkflow::class)->id());
+        $storedChildWorkflow = StoredWorkflow::findOrFail($childWorkflow->id());
+        $storedChildWorkflow->update([
+            'arguments' => Y::serialize([]),
+            'status' => WorkflowPendingStatus::$name,
+        ]);
+        $storedChildWorkflow->parents()->attach($storedWorkflow, [
+            'parent_index' => 0,
+            'parent_now' => now(),
+        ]);
+
+        $workflow = $storedWorkflow->toWorkflow();
+
+        $existingChildWorkflow = ChildWorkflowStub::make(TestChildWorkflow::class)
+            ->then(static function ($value) use (&$result) {
+                $result = $value;
+            });
+
+        $this->assertNull($result);
+    }
+
     public function testAll(): void
     {
         $workflow = WorkflowStub::load(WorkflowStub::make(TestParentWorkflow::class)->id());
