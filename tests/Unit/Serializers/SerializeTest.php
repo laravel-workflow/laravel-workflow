f8f74cb4a85d4897a3b5890a68cfae80e4735589
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-03-23 07:33:15 +0000

    Refactor test

--- tests/Unit/Serializers/SerializeTest.php
+++ tests/Unit/Serializers/SerializeTest.php
@@ -55,11 +55,6 @@ final class SerializeTest extends TestCase
             'int(0)' => [0],
             'int(1)' => [1],
             'exception' => [new \Exception('test')],
-            'exception w/ closure' => [call_user_func(static function () {
-                $e = new \Exception('test');
-                $e->fn = static fn () => null;
-                return $e;
-            })],
             'float PHP_FLOAT_EPSILON' => [PHP_FLOAT_EPSILON],
             'float PHP_FLOAT_MIN' => [PHP_FLOAT_MIN],
             'float -PHP_FLOAT_MIN' => [-PHP_FLOAT_MIN],
