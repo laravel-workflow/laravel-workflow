7e664ff122cb5c839ca2fc49e29af955f81d7efd
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-05-21 00:36:35 +0000

    Update exception class

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -65,7 +65,7 @@ final class ActivityTest extends TestCase
 
     public function testFailedActivity(): void
     {
-        $this->expectException(\Spatie\ModelStates\Exceptions\TransitionNotFound::class);
+        $this->expectException(\Exception::class);
 
         $workflow = WorkflowStub::load(WorkflowStub::make(TestWorkflow::class)->id());
         $activity = new TestExceptionActivity(0, now()->toDateTimeString(), StoredWorkflow::findOrFail(
