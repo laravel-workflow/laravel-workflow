4fafdba4ab8ff93b2af1e366d1b6342683c0d8f7
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-05-21 04:57:09 +0000

    Cleanup

--- src/Middleware/WorkflowMiddleware.php
+++ src/Middleware/WorkflowMiddleware.php
@@ -4,23 +4,10 @@ declare(strict_types=1);
 
 namespace Workflow\Middleware;
 
-use Illuminate\Queue\Events\JobProcessing;
-use Illuminate\Support\Facades\Queue;
-use Workflow\Exception;
-
 final class WorkflowMiddleware
 {
-    private $job;
-
-    private $active = true;
-
     public function handle($job, $next): void
     {
-        $this->job = $job;
-
-        Queue::before(fn (JobProcessing $event) => $this->active = $job->job->getJobId() === $event->job->getJobId());
-        Queue::stopping(fn () => $this->active ? $this->throwExceptionIfActive() : null);
-
         $result = $next($job);
 
         try {
@@ -31,19 +18,5 @@ final class WorkflowMiddleware
                 $job->release();
             }
         }
-    }
-
-    public function throwExceptionIfActive()
-    {
-        $workflow = $this->job->storedWorkflow->toWorkflow();
-
-        Exception::dispatch(
-            $this->job->index,
-            $this->job->now,
-            $this->job->storedWorkflow,
-            new \Exception('Activity timed out.'),
-            $workflow->connection(),
-            $workflow->queue()
-        );
     }
 }
