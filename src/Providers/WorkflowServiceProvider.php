8a981e345d156f0fde0d4d70907db7203f09a9c4
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-06-01 15:55:20 -0500

    Fix serialization (#97)
    * Fix serialization

--- src/Providers/WorkflowServiceProvider.php
+++ src/Providers/WorkflowServiceProvider.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Workflow\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\SerializableClosure\SerializableClosure;
 
 final class WorkflowServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        SerializableClosure::setSecretKey(config('app.key'));
+
         $this->publishes([
             __DIR__ . '/../config/workflows.php' => config_path('workflows.php'),
         ], 'config');
