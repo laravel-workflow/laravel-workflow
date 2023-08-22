cf7c6185f076f49d3f2b8bc25b12461271475a59
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-08-22 23:59:19 +0000

    Add async

--- src/AsyncWorkflow.php
+++ src/AsyncWorkflow.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Workflow;
 
+use Generator;
+
 final class AsyncWorkflow extends Workflow
 {
     public function execute($callback)
     {
-        return yield from ($callback->getClosure())();
+        $coroutine = ($callback->getClosure())();
+        return ($coroutine instanceof Generator) ? yield from $coroutine : $coroutine;
     }
 }
