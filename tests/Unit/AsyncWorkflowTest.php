5d35ee7adb548e09cc1f145a3a39e2d172913239
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-08-23 00:07:39 +0000

    Add async

--- tests/Unit/AsyncWorkflowTest.php
+++ tests/Unit/AsyncWorkflowTest.php
@@ -7,7 +7,6 @@ namespace Tests\Unit;
 use Laravel\SerializableClosure\SerializableClosure;
 use Tests\TestCase;
 use Workflow\AsyncWorkflow;
-use Workflow\Models\StoredWorkflow;
 use Workflow\WorkflowStub;
 
 final class AsyncWorkflowTest extends TestCase
@@ -15,8 +14,8 @@ final class AsyncWorkflowTest extends TestCase
     public function testWorkflow(): void
     {
         $workflow = WorkflowStub::load(WorkflowStub::make(AsyncWorkflow::class)->id());
-        $storedWorkflow = StoredWorkflow::findOrFail($workflow->id());
         $workflow->start(new SerializableClosure(static fn () => 'test'));
+
         $this->assertSame('test', $workflow->output());
     }
 }
