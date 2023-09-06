ab7df2d2531935e00ca9e5391fde7f3b38133dbd
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-09-06 17:28:44 +0000

    Resolve dependencies

--- src/AsyncWorkflow.php
+++ src/AsyncWorkflow.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Workflow;
 
 use Generator;
+use ReflectionFunction;
 
 final class AsyncWorkflow extends Workflow
 {
     public function execute($callback)
     {
-        $coroutine = ($callback->getClosure())();
+        $callable = $callback->getClosure();
+        $coroutine = $callable(...$this->resolveMethodDependencies([], new ReflectionFunction($callable)));
         return ($coroutine instanceof Generator) ? yield from $coroutine : $coroutine;
     }
 }
