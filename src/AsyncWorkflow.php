3b8296bed5ccd1b34185e9aad01f9acd3955c6ce
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-09-06 18:23:14 +0000

    Resolve dependencies

--- src/AsyncWorkflow.php
+++ src/AsyncWorkflow.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Workflow;
 
 use Generator;
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\App;
 use ReflectionFunction;
 
 final class AsyncWorkflow extends Workflow
 {
     public function execute($callback)
     {
+        $this->container = App::make(Container::class);
         $callable = $callback->getClosure();
         $coroutine = $callable(...$this->resolveMethodDependencies([], new ReflectionFunction($callable)));
         return ($coroutine instanceof Generator) ? yield from $coroutine : $coroutine;
