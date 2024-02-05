f451a382c9911157b9ed339225729fbb9dd3ab64
Erik Anders <5753604+Naugrimm@users.noreply.github.com>
2024-02-05 09:49:01 +0100

    fix: change exception column to longtext

---
+++ src/migrations/2024_02_05_093552_change_exception_column_to_longtext.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('workflow_exceptions', function (Blueprint $table) {
+            $table->longText('exception')->change();
+        });
+    }
+};
