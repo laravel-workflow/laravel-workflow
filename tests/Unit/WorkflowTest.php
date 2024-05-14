d00f4886936f6b1fe64df69027310f05df4c9daf
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2024-05-14 01:02:03 +0000

    Add test

--- tests/Unit/WorkflowTest.php
+++ tests/Unit/WorkflowTest.php
@@ -108,4 +108,59 @@ final class WorkflowTest extends TestCase
         $this->assertSame(WorkflowCompletedStatus::class, $childWorkflow->status());
         $this->assertSame('other', $childWorkflow->output());
     }
+
+    public function testParentPending(): void
+    {
+        Carbon::setTestNow('2022-01-01');
+
+        $parentWorkflow = WorkflowStub::load(WorkflowStub::make(TestParentWorkflow::class)->id());
+
+        $storedParentWorkflow = StoredWorkflow::findOrFail($parentWorkflow->id());
+        $storedParentWorkflow->arguments = Y::serialize([]);
+        $storedParentWorkflow->status = WorkflowPendingStatus::class;
+        $storedParentWorkflow->save();
+
+        $storedParentWorkflow->logs()
+            ->create([
+                'index' => 0,
+                'now' => now(),
+                'class' => TestChildWorkflow::class,
+                'result' => Y::serialize('child_workflow'),
+            ]);
+
+        $storedParentWorkflow->logs()
+            ->create([
+                'index' => 1,
+                'now' => now(),
+                'class' => TestActivity::class,
+                'result' => Y::serialize('activity'),
+            ]);
+
+        $childWorkflow = WorkflowStub::load(WorkflowStub::make(TestChildWorkflow::class)->id());
+
+        $storedChildWorkflow = StoredWorkflow::findOrFail($childWorkflow->id());
+        $storedChildWorkflow->arguments = Y::serialize([]);
+        $storedChildWorkflow->status = WorkflowPendingStatus::class;
+        $storedChildWorkflow->save();
+        $storedChildWorkflow->parents()
+            ->attach($storedParentWorkflow, [
+                'parent_index' => 0,
+                'parent_now' => now(),
+            ]);
+
+        $storedChildWorkflow->logs()
+            ->create([
+                'index' => 0,
+                'now' => now(),
+                'class' => TestOtherActivity::class,
+                'result' => Y::serialize('other'),
+            ]);
+
+        (new (TestChildWorkflow::class)($storedChildWorkflow))->handle();
+        (new (TestParentWorkflow::class)($storedParentWorkflow))->handle();
+
+        $this->assertSame('2022-01-01 00:00:00', WorkflowStub::now()->toDateTimeString());
+        $this->assertSame(WorkflowCompletedStatus::class, $childWorkflow->status());
+        $this->assertSame('other', $childWorkflow->output());
+    }
 }
