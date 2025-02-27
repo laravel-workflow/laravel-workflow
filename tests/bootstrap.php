5cf673e05e8d8f37bb1691e42bb69a72300b6f99
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2025-02-27 19:52:31 +0000

    Update testbench

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../classAliases.php';
+require_once __DIR__ . '/classAliases.php';
 
 use PHPUnit\Event\Facade;
 use Tests\TestSuiteSubscriber;
