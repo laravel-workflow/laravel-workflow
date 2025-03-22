28950b54db1fd68a1414bddff012bbc756c9a1f7
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2025-03-22 22:12:43 +0000

    Improve test coverage

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/classAliases.php';
 
 use PHPUnit\Event\Facade;
 use Tests\TestSuiteSubscriber;
