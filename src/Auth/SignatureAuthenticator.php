634bc7123f5ad70ec12836caa0fac202caff95e5
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2025-04-12 19:57:13 +0000

    Add custom class

--- src/Auth/SignatureAuthenticator.php
+++ src/Auth/SignatureAuthenticator.php
@@ -11,7 +11,7 @@ class SignatureAuthenticator implements WebhookAuthenticator
     public function validate(Request $request): bool
     {
         return hash_equals(
-            $request->header(config('workflows.webhook_auth.signature.header')),
+            $request->header(config('workflows.webhook_auth.signature.header')) ?? '',
             hash_hmac('sha256', $request->getContent(), config('workflows.webhook_auth.signature.secret'))
         );
     }
