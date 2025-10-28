eaf6822cb7c748340155619ead077034bd61a153
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2025-10-28 16:03:38 +0000

    Tests

---
+++ tests/Fixtures/TestExceptionActivity.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixtures;
+
+use Exception;
+use Workflow\Activity;
+
+final class TestExceptionActivity extends Activity
+{
+    public $tries = 1;
+
+    public function execute($shouldThrow)
+    {
+        if ($shouldThrow) {
+            throw new Exception('failed');
+        }
+
+        return 'activity';
+    }
+}
