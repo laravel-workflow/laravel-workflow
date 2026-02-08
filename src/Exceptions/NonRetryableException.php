30ac20167cf4bebc62c1f392dc0425cc7e2cf3ba
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2026-02-08 08:48:47 -0600

    Use nullable (#319)

--- src/Exceptions/NonRetryableException.php
+++ src/Exceptions/NonRetryableException.php
@@ -9,7 +9,7 @@ use Throwable;
 
 class NonRetryableException extends Exception implements NonRetryableExceptionContract
 {
-    public function __construct($message = '', $code = 0, Throwable $previous = null)
+    public function __construct($message = '', $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
